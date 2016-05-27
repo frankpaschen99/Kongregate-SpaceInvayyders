7ff37393b255a363f49d2afeb5e66a13df4822f4
Frank Paschen <frankpaschen99@gmail.com>
2016-05-26 23:40:15 -0400

    Pretty shit so far. Gotta learn more about js to continue

--- game/loadGame.js
+++ game/loadGame.js
@@ -15,7 +15,9 @@ LoadGame.prototype = {
     
 	// Load various image, atlas resources 
     loadImages: function () {
-		
+		game.load.image('player_sprite', 'js/game/assets/actors/player_ship.png');
+		game.load.image('player_laser', 'js/game/assets/entities/laser.png');
+		game.load.image('scene_backdrop', 'js/game/assets/ui/space_background.png');
     },
     
     loadFonts: function () {
--- game/mainGame.js
+++ game/mainGame.js
@@ -1,17 +1,59 @@
-// The actual game will be scripted here
+var player;
 
 var MainGame = function () {};
 MainGame.prototype = {
-    switchFull: function () {
-        game.scale.isFullScreen ? game.scale.stopFullScreen() : game.scale.startFullScreen(false);
-    },
-    
     create: function () {
-        var pauseKey = game.input.keyboard.addKey(Phaser.Keyboard.F6);
-        pauseKey.onDown.add(this.switchFull, this);
+		var backdrop = game.add.sprite(0, 0, 'scene_backdrop');
+		player = new Player(0, 0);
     },
     
     update: function () {
-    
+		player.update(game.time.elapsed);
     }
 };
+
+class Laser {
+	constructor(posX, posY) {
+		this.sprite = game.add.sprite(posX, posY, 'player_laser');
+		this.fire();
+	}
+	fire() {
+		this.sprite.y -= 100;
+	}
+}
+class Player {
+	constructor(posX, posY) {
+		this.hp = 100;
+		this.x = posX;
+		this.y = (800-100) - posY;	// dont you love magic constants? its window height minus sprite height minus specified Y pos
+		this.sprite = game.add.sprite(this.x, this.y, 'player_sprite');
+		this.space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		this.space.onDown.add(v => {
+			this.fire();
+		}, this);
+	}
+	update(deltaTime) {
+		// window borders for sprite
+		if (this.sprite.x < 0) this.sprite.x = 1;
+		else if (this.sprite.x > 538) this.sprite.x = 537;
+		
+		var speed = 0.25 * deltaTime;
+		
+		// if-elif here so they cant use A and D at same time
+		if (game.input.keyboard.isDown(Phaser.Keyboard.A)) {
+			this.sprite.x -= speed;
+		} else if (game.input.keyboard.isDown(Phaser.Keyboard.D)) {
+			this.sprite.x += speed;
+		}
+	}
+	fire(deltaTime)  {
+		var s = new Laser(this.sprite.x, this.sprite.y);
+		
+	}
+	damage(amount) {
+		this.hp -= amount;
+	}
+	die() {
+		// Game over
+	}
+}
